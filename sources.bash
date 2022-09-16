cfaaf1b51bd28e3cbacabd2bcf661b0093b0cf29
ros <christian.mock.chrimo@googlemail.com>
2022-09-16 21:08:07 +0200

    sourcef file for models and worlds updated

--- sources.bash
+++ sources.bash
@@ -1,5 +1,9 @@
+export basedir=/home/ros/prog
+export catkindir=$based/catkin_openmower_simulator_ws
 source /opt/ros/noetic/setup.bash
 source /usr/share/gazebo/setup.sh
-source ./devel/setup.bash
-export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:$HOME/prog/gazebo_models_worlds_collection/worlds:$HOME/prog/gazebo_models_worlds_collection/models:.
+source $catkindir/devel/setup.bash
+
+export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:$basedir/gazebo_models_worlds_collection/worlds:$basedir/gazebo_models_worlds_collection/models:.
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$basedir/gazebo_models_worlds_collection/models
 
